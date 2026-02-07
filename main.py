82c472e486e9debef02eb6235ac47b56c6a46769
deltagabv2 <gabrielfortoul4@gmail.com>
2026-02-07 10:48:46 +0100

    Connection + affichage bdd mise en page projet,index.html

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, redirect
-import pymongo
+from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 # Connection à la BDD
+
+mongo = os.getenv("MONGO_URI")
+client = MongoClient(mongo)
+
+db = client.get_database("SmartDev")
 
 
 
@@ -15,6 +22,7 @@ def index():
 
 @app.route("/projet")
 def projet():
-    return render_template("front/projet.html")
+    projet_data = list(db["projet"].find({}))
+    return render_template("front/projet.html", projet = projet_data)
 
 app.run(host="0.0.0.0", port=32768)
