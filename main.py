0c6c9bed508156b930f640b7220b0e1133a666cc
deltagabv2 <gabrielfortoul4@gmail.com>
2026-03-14 10:44:53 +0100

    Creation page login (fonctionel) + register (erreur bcrypt : register)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+import bcrypt
 
 load_dotenv()
 
@@ -13,6 +14,7 @@ mongo = os.getenv("MONGO_URI")
 client = MongoClient(mongo)
 
 db = client.get_database("SmartDev")
+app.secret_key = os.urandom(24)
 
 
 
@@ -24,5 +26,55 @@ def index():
 def projet():
     projet_data = list(db["projet"].find({}))
     return render_template("front/projet.html", projet = projet_data)
+
+@app.route("/publish")
+def publish():
+    return render_template("front/publish.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        db_users = db["user"]
+        user = db_users.find_one({"username" : request.form["user"]})
+        if user:
+            if bcrypt.checkpw(request.form["mdp"].encode("utf-8"), user["password"]):
+                session["user"] = user["user"]
+                session["role"] = user["role"]
+                return redirect("/projet")
+            else:
+                return render_template("front/login.html", erreur = "Erreur : Mot de Passe Incorrect")
+        else:
+            return render_template("front/login.html", erreur = "Erreur : Nom d'Utilisateur Incorrect")    
+    else:
+        return render_template("front/login.html")  
+
+@app.route('/register', methods = ['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        db_users = db["user"]
+        if(db_users.find_one({"username" : request.form['user']})):
+            return render_template('register.html', erreur = "le pseudo est déjà utilisé")
+        else : 
+            if(request.form["mdp"] == request.form['confirm_mdp']):
+                utilisateur = request.form['user']
+                mdp = request.form['mdp']
+
+                #Hash du mot de passe
+                mdp_crypte = mdp.encode("utf-8")
+                salt = bcrypt.gensalt()
+                mdp_hash = bcrypt.hashpw(mdp_crypte, salt)
+
+                new_user = {
+                    "username" : utilisateur,
+                    "password" : mdp_hash,
+                    "role" : "user",
+                }
+
+                db["user"].insert_one(new_user)
+                return redirect("/")
+            else : 
+                return render_template('front/register.html', erreur = "les mots de passe ne correspondes pas")
+    else:
+        return render_template('front/register.html')
 
 app.run(host="0.0.0.0", port=32768)
